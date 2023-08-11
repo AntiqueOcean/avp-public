731143874701decf6cc3ff65016835ba875447e2
Antique Ocean <antiqueocean.dev@gmail.com>
2023-08-11 14:34:47 +0330

    0005_0_01_alpha
    completed new look

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+setInterval(function() {
+    
+    const percent = Math.min((document.documentElement.scrollTop / (document.documentElement.offsetHeight - window.innerHeight) * 100), 100);
+    document.getElementById("scrollBar").style.width =  percent + '%';
+    document.getElementById("scrollPercent").innerHTML = Math.round(percent) + '%';
+}, 250);
+
+function scrollto(input){
+    const element = document.getElementById(input);
+    const y = element.getBoundingClientRect().top + window.pageYOffset - 90;
+    window.scrollTo({top: y, behavior: 'smooth'});
+
+}
