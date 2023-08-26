dad93e36d719a66dfc5a710d7558fab9368bf630
Antique Ocean <antiqueocean.dev@gmail.com>
2023-08-26 10:57:37 +0330

    0017_0_01_02_alpha
    added v0.1.2-alpha

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
-setInterval(function() {
+window.addEventListener("scroll", function() {
     
     const percent = Math.min((document.documentElement.scrollTop / (document.documentElement.offsetHeight - window.innerHeight) * 100), 100);
     document.getElementById("scrollBar").style.width =  percent + '%';
     document.getElementById("scrollPercent").innerHTML = Math.round(percent) + '%';
-}, 250);
+});
 
 function scrollto(input){
     const element = document.getElementById(input);
